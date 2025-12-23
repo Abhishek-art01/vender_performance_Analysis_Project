70c9dede77f779480dbda1a505432aff18e03824
Aarambh <aarambh@gmail.com>
2025-12-24 02:59:46 +0530

    clened some datd

---
+++ module.py
@@ -0,0 +1,8 @@
+import time
+def connect() -> None:
+    print("Connecting to internet")
+    time.sleep(3)
+    print("you are now connected to internet")
+
+if __name__ == "__main__":
+    connect()
